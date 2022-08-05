b764ef4e7f318514160236430f475a6ef5d42580
limjongseung <jongseungim5348@gmail.com>
2022-08-05 13:34:51 +0900

    portfolioi_day03

---
+++ Portfolio_selex/js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+
+//     $("#header").hover(function(){
+//         $("h1").hide();
+//         $(".logo_black").show();
+//     },function(){
+//         $("h1").show();
+//         $(".logo_black").hide();
+//     });
+
+// });
+

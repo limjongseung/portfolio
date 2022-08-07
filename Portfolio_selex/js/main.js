14be3c03d54a4a069ed40ddaab175d6bf2a71391
limjongseung <jongseungim5348@gmail.com>
2022-08-07 17:30:50 +0900

    portfolio_day06

--- Portfolio_selex/js/main.js
+++ Portfolio_selex/js/main.js
@@ -2,8 +2,9 @@
 
 
 
-$(function(){
 
+$(function(){
+    
     $("#header").on({
         mouseenter : function(){
             $("h1 img").attr("src","img/selex_black.png");
@@ -13,28 +14,25 @@ $(function(){
         }
     });
     
-
+    
     $("#header .header_wrap .hamburger").click(function(){
-      $("#header .header_wrap .hamburger>span").toggleClass("on");
-       $(".mega_menu").toggleClass("active");
-       
+        $("#header .header_wrap .hamburger>span").toggleClass("on");
+        $(".mega_menu").toggleClass("active");
+        
     });
 });
 
-// let verticalBar = document.getElementByClassName("vertical_underline");
-// let verticalMenus = document.querySelectorAll("nav>li a");
-
-// function verticalIndicator(e) {
-//   verticalBar.style.left = e.offsetLeft + "px";
-//   verticalBar.style.width = e.offsetWidth + "px";
-//   verticalBar.style.top = e.offsetTop + e.offsetHeight + "px";
-// }
-
-// verticalMenus.forEach((menu) =>
-//   menu.addEventListener("mouseover", (e) => verticalIndicator(e.currentTarget))
-// );
-
-
+let verticalBar = document.getElementById("vertical_underline");
+let verticalMenus = document.querySelectorAll(".mega_menu .mega_gnb ul>li a");
+function verticalIndicator(e) {
+    verticalBar.style.left = e.offsetLeft  + "px";
+    verticalBar.style.width = e.offsetWidth + "px";
+    verticalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+  }
+  
+  verticalMenus.forEach((menu) =>
+    menu.addEventListener("mouseover", (e) => verticalIndicator(e.currentTarget))
+  );
 
 
 

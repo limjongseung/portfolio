791c91f61e66e9cea304f0e4b9089eadf3bf4056
limjongseung <jongseungim@gmail.com>
2022-08-26 17:50:40 +0900

    포트폴리오_day04

--- hdex/js/main.js
+++ hdex/js/main.js
@@ -1,4 +1,53 @@
+$(function(){
+
+    $("#main .con_10 .con_list>li .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+
+});
+
+
+
+
 $(document).on("scroll",function(){
     let scrollTop = $(document).scrollTop();
     $(".scroll_box").text(scrollTop);
+
+    $("#main .con_4 .img_wrap1 .img_elipse img").css("transform","rotate("+ scrollTop/4 + "deg");
+
+
+    if( scrollTop>=833 ) {
+        $("#main .con_2 .img_wrap").addClass("on");
+    }
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

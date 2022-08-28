0ec6153cfdb40d71f09fcc10b5837367c56fcdc9
lim5348 <jongseungim5348@gmail.com>
2022-08-28 11:37:34 +0900

    포트폴리오

--- hdex/js/main.js
+++ hdex/js/main.js
@@ -1,5 +1,45 @@
-$(function(){
+//스크롤 이벤트
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    $(".scroll_box").text(scrollTop);
+    $("#main .con_4 .img_wrap1 .img_elipse img").css("transform","rotate("+ scrollTop/3 + "deg");
+    $("#main .con_band .img_wrap img").css("transform","translateX(" + -scrollTop/1.5 + "px");
+    if( scrollTop>=800 ) {
+        $("#main .con_2 .img_wrap").addClass("on");
+        $("#main .con_2  .text_wrap .text_wrap_1").addClass("on");
+    };
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 상품색상 육각형 호버 효과
+$(function(){
     $("#main .con_10 .con_list>li .color_box>li").mouseenter(function(){
         $("#main .con_10 .con_list>li .color_box>li").not($(this)).addClass("low");
         $(this).addClass("up");
@@ -8,30 +48,7 @@ $(function(){
         $("#main .con_10 .con_list>li .color_box>li").removeClass("low");
         $(this).removeClass("up");
     });
-
 });
-
-
-
-
-$(document).on("scroll",function(){
-    let scrollTop = $(document).scrollTop();
-    $(".scroll_box").text(scrollTop);
-
-    $("#main .con_4 .img_wrap1 .img_elipse img").css("transform","rotate("+ scrollTop/4 + "deg");
-
-
-    if( scrollTop>=833 ) {
-        $("#main .con_2 .img_wrap").addClass("on");
-    }
-
-
-
-
-
-});
-
-
 
 
 

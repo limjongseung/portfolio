a0f64b4dc955870bcfb48aba6234c290e509bb26
limjongseung <jongseungim5348@gmail.com>
2022-08-28 14:27:51 +0900

    포트폴리오_day03

--- hdex/js/main.js
+++ hdex/js/main.js
@@ -4,11 +4,77 @@ $(document).on("scroll",function(){
     $(".scroll_box").text(scrollTop);
     $("#main .con_4 .img_wrap1 .img_elipse img").css("transform","rotate("+ scrollTop/3 + "deg");
     $("#main .con_band .img_wrap img").css("transform","translateX(" + -scrollTop/1.5 + "px");
-    if( scrollTop>=800 ) {
+    if( scrollTop>=800) {
         $("#main .con_2 .img_wrap").addClass("on");
-        $("#main .con_2  .text_wrap .text_wrap_1").addClass("on");
-    } else if (scrollTop >=900){
-        $("main")
+        $("#main .con_2  .text_wrap .text_wrap_1 .text_e").addClass("on");
+    }
+});
+
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 850){
+            $("#main .con_2  .text_wrap .text_wrap_2").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 1400){
+            $("#main .con_3  .img_wrap>p:first-child").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 1430){
+            $("#main .con_3  .img_wrap .img_text").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 1550){
+            $("#main .con_3 .text_wrap .text_wrap_main").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 1580){
+            $("#main .con_3 .text_wrap .text_wrap_link").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 2100){
+            $("#main .con_4 .img_wrap2 p").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 2400){
+            $("#main .con_4 .img_wrap1 .img_wrap_scroll p").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 2500){
+            $("#main .con_4 .img_wrap1 .img_text").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 4000){
+            $("#main .con_5 .img_wrap .logo_wrap").addClass("on")
+            $("#main .con_5 .img_wrap>p").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 4600){
+            $("#main .con_5 .text_wrap .text_scroll").addClass("on")
+    }
+});
+$(document).on("scroll",function(){
+    let scrollTop = $(document).scrollTop();
+    if(scrollTop >= 5200){
+            $("#main .con_6 .img_wrap1 p").addClass("on")
     }
 });
 

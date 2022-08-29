d38377c09e1696f24230471614c0508f5ae99b09
lim5348 <jongseungim5348@gmail.com>
2022-08-29 13:32:42 +0900

    포트폴리오

--- hdex/js/main.js
+++ hdex/js/main.js
@@ -129,13 +129,278 @@ $(document).on("scroll",function(){
 
 // 상품색상 육각형 호버 효과
 $(function(){
-    $("#main .con_10 .con_list>li .color_box>li").mouseenter(function(){
-        $("#main .con_10 .con_list>li .color_box>li").not($(this)).addClass("low");
+    $("#main .con_10 .con_list>li:nth-child(1) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(1) .color_box>li").not($(this)).addClass("low");
         $(this).addClass("up");
     });
-    $("#main .con_10 .con_list>li .color_box>li").mouseleave(function(){
-        $("#main .con_10 .con_list>li .color_box>li").removeClass("low");
+    $("#main .con_10 .con_list>li:nth-child(1) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(1) .color_box>li").removeClass("low");
         $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(2) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(2) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(2) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(2) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(3) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(3) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(3) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(3) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(4) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(4) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(5) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(5) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(5) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(5) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box>li").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box>li").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(1) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(1) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(1) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(1) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(3) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(3) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(3) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(3) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(4) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(4) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(4) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(4) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(5) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(5) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(5) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(5) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(6) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(6) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(6) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(6) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(7) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(7) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(7) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(7) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+$(function(){
+    $("#main .con_12 .con_list>li:nth-child(8) .color_box>li").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(8) .color_box>li").not($(this)).addClass("low");
+        $(this).addClass("up");
+    });
+    $("#main .con_12 .con_list>li:nth-child(8) .color_box>li").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(8) .color_box>li").removeClass("low");
+        $(this).removeClass("up");
+    });
+});
+
+// 육각형 호버시 상품이미지가 바뀌는 구동
+$(function(){
+    $("#main .con_10 .con_list>li:nth-child(1) .color_box>li:nth-child(1)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(1) .img_wrap img").attr("src", "img/kim_con1.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(1) .color_box>li:nth-child(2)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(1) .img_wrap img").attr("src", "img/compression_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(2) .color_box>li:nth-child(1)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/arch_logo.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(2) .color_box>li:nth-child(2)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/kim_con2.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(3) .color_box>li:nth-child(1)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(3) .img_wrap img").attr("src", "img/compression_sleeveless_black.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(3) .color_box>li:nth-child(2)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(3) .img_wrap img").attr("src", "img/compression_sleeveless_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(3) .color_box").mouseleave(function(){
+            $("#main .con_10 .con_list>li:nth-child(3) .img_wrap img").attr("src", "img/kim_con3.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box>li:nth-child(1)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(4) .img_wrap img").attr("src", "img/kim_con4.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box>li:nth-child(2)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(4) .img_wrap img").attr("src", "img/sleeveless_black.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box>li:nth-child(3)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(4) .img_wrap img").attr("src", "img/sleeveless_blue.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(5) .color_box>li:nth-child(1)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(5) .img_wrap img").attr("src", "img/camo_muscle_black.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(5) .color_box>li:nth-child(2)").mouseenter(function(){
+            $("#main .con_10 .con_list>li:nth-child(5) .img_wrap img").attr("src", "img/camo_muscle_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(5) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(5) .img_wrap img").attr("src", "img/kim_con5.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li:nth-child(1)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/main_logo_black.png");
+   });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li:nth-child(2)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/main_logo_blue.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li:nth-child(3)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/main_logo_gray.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li:nth-child(4)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/main_logo_black_gray.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box>li:nth-child(5)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/main_logo_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(6) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(6) .img_wrap img").attr("src", "img/kim_con6.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(1) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(1) .img_wrap img").attr("src", "img/kim_con1.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(2) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/kim_con2.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(4) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(4) .img_wrap img").attr("src", "img/kim_con4.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li:nth-child(1)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .img_wrap img").attr("src", "img/back_black.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li:nth-child(2)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .img_wrap img").attr("src", "img/back_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li:nth-child(3)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .img_wrap img").attr("src", "img/back_blue.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box>li:nth-child(4)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .img_wrap img").attr("src", "img/back_khaki.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(7) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(7) .img_wrap img").attr("src", "img/kim_con7.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box>li:nth-child(1)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .img_wrap img").attr("src", "img/camo_black.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box>li:nth-child(2)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .img_wrap img").attr("src", "img/camo_white.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box>li:nth-child(3)").mouseenter(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .img_wrap img").attr("src", "img/camo_khaki.png");
+    });
+    $("#main .con_10 .con_list>li:nth-child(8) .color_box").mouseleave(function(){
+        $("#main .con_10 .con_list>li:nth-child(8) .img_wrap img").attr("src", "img/kim_con8.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li:nth-child(1)").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_arch_black.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li:nth-child(2)").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_arch_white.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li:nth-child(3)").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_arch_navy.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li:nth-child(4)").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_arch_blue.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box>li:nth-child(5)").mouseenter(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_arch_green.png");
+    });
+    $("#main .con_12 .con_list>li:nth-child(2) .color_box").mouseleave(function(){
+        $("#main .con_12 .con_list>li:nth-child(2) .img_wrap img").attr("src", "img/heo_con2.png");
     });
 });
 
